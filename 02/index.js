c864d767628c666a77ec8fd83b709c260bda38fb
voja <voja@gmail.com>
2022-03-12 12:56:19 +0100

    2 4 Static content

--- 02/index.js
+++ 02/index.js
@@ -3,6 +3,8 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     // res.json({
     //     course: "Node for beginners",
