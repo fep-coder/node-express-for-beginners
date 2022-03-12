49a079be6ea903b2423b46297b87f19e1072d64c
voja <voja@gmail.com>
2022-03-12 12:41:39 +0100

    2 3 Express routing

--- 02/index.js
+++ 02/index.js
@@ -1,5 +1,21 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+app.get("/", (req, res) => {
+    // res.json({
+    //     course: "Node for beginners",
+    // });
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "about.html"));
+});
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, "contact.html"));
+});
 
 app.listen(3000, console.log("Listening on port 3000"));
