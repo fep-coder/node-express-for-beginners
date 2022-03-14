4891264ea6672d16aaef0fd352cc4ad78a21de2e
voja <voja@gmail.com>
2022-03-14 09:12:19 +0100

    3 2 Routes setup

--- blog/index.js
+++ blog/index.js
@@ -4,6 +4,18 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.sendFile("pages/index.html", { root: "public" });
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile("pages/about.html", { root: "public" });
+});
+
+app.get("/contact", (req, res) => {
+    res.sendFile("pages/contact.html", { root: "public" });
+});
+
 app.listen(3000, () => {
     console.log("App listening on port 3000");
 });
