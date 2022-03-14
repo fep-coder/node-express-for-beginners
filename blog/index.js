77ded38f1af005722e1cc6dbf6eeb256d6f562b1
voja <voja@gmail.com>
2022-03-14 11:31:28 +0100

    3 8 CREATE post

--- blog/index.js
+++ blog/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 mongoose.connect("mongodb://localhost/blog");
 
+const Post = require("./models/Post");
+
 const ejs = require("ejs");
 app.set("view engine", "ejs");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
@@ -20,6 +26,21 @@ app.get("/about", (req, res) => {
 
 app.get("/contact", (req, res) => {
     res.render("contact");
+});
+
+app.get("/posts/create", (req, res) => {
+    res.render("create");
+});
+
+// app.post("/posts/create", (req, res) => {
+//     Post.create(req.body, (error, post) => {
+//         res.redirect("/");
+//     });
+// });
+
+app.post("/posts/create", async (req, res) => {
+    await Post.create(req.body);
+    res.redirect("/");
 });
 
 app.listen(3000, () => {
