989fd992d01276af63830b119d6a92d18ced3ba6
voja <voja@gmail.com>
2022-03-17 09:19:01 +0100

    3 16 Password hashing

--- blog/models/User.js
+++ blog/models/User.js
@@ -1,9 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const UserSchema = new Schema({
     username: String,
     password: String,
+});
+
+UserSchema.pre("save", function (next) {
+    bcrypt.hash(this.password, 10, (error, hash) => {
+        this.password = hash;
+        next();
+    });
 });
 
 const User = mongoose.model("User", UserSchema);
--- blog/routes/users.js
+++ blog/routes/users.js
@@ -7,4 +7,9 @@ router.get("/register", (req, res) => {
     res.render("register");
 });
 
+router.post("/register", async (req, res) => {
+    await User.create(req.body);
+    res.redirect("/");
+});
+
 module.exports = router;
